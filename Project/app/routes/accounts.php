37b306784fd853d4a9322b4497d5fdccf9655ccf
NexRX <0b8cinsert>
2020-11-26 13:14:53 +0000

    Added support for dynamic navigation bar buttons which now relate to if the user is logged in or not.

--- Project/app/routes/accounts.php
+++ Project/app/routes/accounts.php
@@ -13,22 +13,31 @@ use Monolog\Handler\StreamHandler;
 $app->get('/login', function(Request $request, Response $response) {
     session_start();
 
-    /* End */
+    if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {               //TODO Make a class that returns this so it can be called dynamically elsewhere
+        $twigsArray = [
+            'username' => $_SESSION['USERNAME'],
+            'loginFirstLink' =>  "#",
+            'loginFirstText' => "Placeholder",
+            'loginSecondLink' =>  "/logout",
+            'loginSecondText' => "Logout",
+        ];
+    } else {
+        $twigsArray = [
+            'username' => "Accounts",
+            'loginFirstLink' =>  "/login",
+            'loginFirstText' => "Login",
+            'loginSecondLink' =>  "/register",
+            'loginSecondText' => "Register",
+        ];
+    }
 
-    return $this->view->render(
-        $response,
-        'login.html.twig',
-        [
-            // Login navigation
-            'username' => $_SESSION['UID'],
-        ]
-    );
+    return $this->view->render( $response,'login.html.twig', $twigsArray);
 })->setName('Login');
 
 $app->post('/auth-login', function (Request $request, Response $response) {
     session_start();
 
-    /* Error Logging */
+    /* Error Logging */                                 //TODO Convert to an object or function with a parameter to set the log file name and then give it a correct path.
     $logs_file_path = __DIR__ . '/private_logs/';
     $logs_file_name = 'auth-login.log';
     $logs_file = $logs_file_path . $logs_file_name;
@@ -39,39 +48,44 @@ $app->post('/auth-login', function (Request $request, Response $response) {
 
     $params = $request->getParsedBody();
 
-    $username = $params['username'];
-    $password = $params['password'];
-
     //Basic check to remove unwanted characters. If invalid characters are present the query is never made for security reasons otherwise the query is made and a result is given.
-    if (preg_match('![()}{/#<>,[\]\\,\'|\x22]+!', $username))
+    if (preg_match('![()}{/#<>,[\]\\,\'|\x22]+!', $params['username']))
     {
         $login_success = "Your username contains special characters not allowed ( ()}{@#/<>,[]\,'|\" )";
-        $log->warning("User posted invalid chars despite form validation. Username: " . $username);
+        $log->warning("User posted invalid chars despite form validation. Username: " . $params['username']);
     } else {
-        $login_success = SQLQ::loginQuery($log, $username, $password);
+        $login_success = SQLQ::loginQuery($log, $params['username'], $params['password']);
     }
 
     // Defining what to tell the user. Success or failure of login.
     if ($login_success == "true")
     {
         $_SESSION['logged_in'] = true;
-        $log->info('Login Success: ' . $username);
+        $log->info('Login Success: ' . $params['username']);
     } else {
         $_SESSION['logged_in'] = false;
-        $log->info('Login Attempt: ' . $username);
+        $log->info('Login Attempt: ' . $params['username']);
     }
 
-    return $this->view->render(
-        $response,
-        'login_results.html.twig',
-        [
-            // Login navigation
-            'username' => $username,
+    if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {               //TODO Make a class that returns this so it can be called dynamically elsewhere
+        $twigsArray = [
+            'username' => $_SESSION['USERNAME'],
+            'loginFirstLink' =>  "#",
+            'loginFirstText' => "Placeholder",
+            'loginSecondLink' =>  "/logout",
+            'loginSecondText' => "Logout",
+        ];
+    } else {
+        $twigsArray = [
+            'username' => "Accounts",
+            'loginFirstLink' =>  "/login",
+            'loginFirstText' => "Login",
+            'loginSecondLink' =>  "/register",
+            'loginSecondText' => "Register",
+        ];
+    }
 
-            // Page vars
-            'login_success' => $login_success,
-        ]
-    );
+    return $this->view->render($response, 'login_results.html.twig', $twigsArray, $login_success);
 })->setName('Authorising Login');
 
 
@@ -81,14 +95,26 @@ $app->post('/auth-login', function (Request $request, Response $response) {
 $app->get('/register', function(Request $request, Response $response) {
     session_start();
 
+    if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {               //TODO Make a class that returns this so it can be called dynamically elsewhere
+        $twigsArray = [
+            'username' => $_SESSION['USERNAME'],
+            'loginFirstLink' =>  "#",
+            'loginFirstText' => "Placeholder",
+            'loginSecondLink' =>  "/logout",
+            'loginSecondText' => "Logout",
+        ];
+    } else {
+        $twigsArray = [
+            'username' => "Accounts",
+            'loginFirstLink' =>  "/login",
+            'loginFirstText' => "Login",
+            'loginSecondLink' =>  "/register",
+            'loginSecondText' => "Register",
+        ];
+    }
+
     /* End */
-    return $this->view->render(
-        $response,
-        'register.html.twig',
-        [
-            // Not data yet. Placeholder.
-        ]
-    );
+    return $this->view->render($response,'register.html.twig', $twigsArray);
 
 })->setName('Register');
 
@@ -96,7 +122,7 @@ $app->post('/auth-register',
     function (Request $request, Response $response) {
         session_start();
 
-        /* Error Logging */
+        /* Error Logging Setup*/                            //TODO Convert to an object or function with a parameter to set the log file name and then give it a correct path.
         $logs_file_path = __DIR__ . '/private_logs/';
         $logs_file_name = 'auth-register.log';
         $logs_file = $logs_file_path . $logs_file_name;
@@ -108,48 +134,52 @@ $app->post('/auth-register',
         /* Gathering post data. */
         $params = $request->getParsedBody();
 
-        $username = $params['username'];
-        $password = $params['password'];
-        $passwordConfirm = $params['password-confirm'];
-        $email = $params['email'];
-
         /* PHP Form validation */
-        if (preg_match('![()}{/#<>,[\]\\,\'|\x22]+!', $username))
+        if (preg_match('![()}{/#<>,[\]\\,\'|\x22]+!', $params['username']))
         {
             $register_success = "Your username contains special characters not allowed ( ()}{#/<>,[]\,'|\" )";
-            $log->warning("User posted invalid chars despite form validation. Username: " . $username);
-        } else if ($password != $passwordConfirm) {
+            $log->warning("User posted invalid chars despite form validation. Username: " . $params['username']);
+        } else if ($params['password'] != $params['password-confirm']) {
             $register_success = "Passwords do not match.";
-        } else if (!filter_var($email, FILTER_VALIDATE_EMAIL) || preg_match('![()}{/#<>,[\]\\,\'|\x22]+!', $email)) {
+        } else if (!filter_var($params['email'], FILTER_VALIDATE_EMAIL) || preg_match('![()}{/#<>,[\]\\,\'|\x22]+!', $params['email'])) {
             $register_success = "Invalid email format / invalid characters.";
-            $log->warning("User posted invalid chars or email format despite form validation. Email: " . $email);
-        } else {
-            /* End */
+            $log->warning("User posted invalid chars or email format despite form validation. Email: " . $params['email']);
+        } else { /* End Validation*/
 
             /* Password Security */
-            $hashedPassword = password_hash($password, PASSWORD_DEFAULT); // Uses strongest at time till php updates then it will use a newer. Older hashes i believe are compatible with password_verify however.
-            password_verify($password, $hashedPassword); // Can be removed but is here to remind you that this is needed to verify passwords with the above function.
+            $hashedPassword = password_hash($params['password'], PASSWORD_DEFAULT); // Uses strongest at time till php updates then it will use a newer method. Older hashes i believe are compatible with password_verify however.
+            password_verify($params['password'], $hashedPassword); // Can be removed but is here to remind you that this is needed to verify passwords when hashing with the above function.
             /* end */
 
-            $register_success = SQLQ::registerQuery($log, $username, $hashedPassword, $email);
+            $register_success = SQLQ::registerQuery($log, $params['username'], $hashedPassword, $params['email']);
 
             if ($register_success) {
                 $_SESSION['logged_in'] = true;
-                $log->notice('Register Success: ' . $username);
+                $log->notice('Register Success: ' . $params['username']);
             } else {
                 $_SESSION['logged_in'] = false;
             }
         }
-        return $this->view->render(
-            $response,
-            'register_results.html.twig',
-            [
-                // Login navigation
-                'username' => $username,
 
-                // Page vars
-                'register_success' => $register_success,
-            ]);
+        if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {               //TODO Make a class that returns this so it can be called dynamically elsewhere
+            $twigsArray = [
+                'username' => $_SESSION['USERNAME'],
+                'loginFirstLink' =>  "#",
+                'loginFirstText' => "Placeholder",
+                'loginSecondLink' =>  "/logout",
+                'loginSecondText' => "Logout",
+            ];
+        } else {
+            $twigsArray = [
+                'username' => "Accounts",
+                'loginFirstLink' =>  "/login",
+                'loginFirstText' => "Login",
+                'loginSecondLink' =>  "/register",
+                'loginSecondText' => "Register",
+            ];
+        }
+
+        return $this->view->render($response, 'register_results.html.twig', $twigsArray, $register_success);
     })->setName('Authorising Registration');
 
 
@@ -158,7 +188,21 @@ $app->post('/auth-register',
 /// Other/Logout ///
 $app->get('/logout', function (Request $request, Response $response) use ($app) {
     session_start();
-    require_once "lib/logout.php"; //Do not give this app Session
+    // Unset all of the session variables.
+    $_SESSION = array();
+
+    // If it's desired to kill the session, also delete the session cookie.
+    // Note: This will destroy the session, and not just the session data!
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+
+    // Finally, destroy the session.
+    session_destroy();
 
     return $this->response->withRedirect('/');
 })->setName('Logout');
--- Project/app/routes/homepage.php
+++ Project/app/routes/homepage.php
@@ -5,9 +5,26 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 $app->get('/', function(Request $request, Response $response)
 {
+    session_start();
+
+    if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {               //TODO Make a class that returns this so it can be called dynamically elsewhere
+        $twigsArray = [
+            'username' => $_SESSION['USERNAME'],
+            'loginFirstLink' =>  "#",
+            'loginFirstText' => "Placeholder",
+            'loginSecondLink' =>  "/logout",
+            'loginSecondText' => "Logout",
+        ];
+    } else {
+        $twigsArray = [
+            'username' => "Accounts",
+            'loginFirstLink' =>  "/login",
+            'loginFirstText' => "Login",
+            'loginSecondLink' =>  "/register",
+            'loginSecondText' => "Register",
+            ];
+    }
+
     return $this->view->render($response,
-    'testhomepage.html.twig',
-    [
-       'test_data' => 'this is a test'
-    ]);
+        'homepage.html.twig', $twigsArray);
 });
