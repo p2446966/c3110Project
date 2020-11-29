b9c18a1e82b41eeb19cc8da3baa4a6bebe65b670
p2446966 <58215244+p2446966@users.noreply.github.com>
2020-11-29 13:25:26 +0000

    Updated settings.php to contain more

--- Project/app/settings.php
+++ Project/app/settings.php
@@ -5,9 +5,23 @@
 define('APP_NAME', 'Project'); //this can be changed
 define('LANDING_PAGE', $_SERVER['SCRIPT_NAME']);
 
+$log_file_path = __DIR__ . '/private_logs/';
+$log_file_login_name = 'auth-login.log';
+$log_file_login_register = 'auth-register.log';
+
+define('LOGIN_LOG', $log_file_path . $log_file_login_name);
+define('REGISTER_LOG', $log_file_path . $log_file_login_register);
+
 $wsdl = 'https://m2mconnect.ee.co.uk/orange-soap/services/MessageServiceByCountry?wsdl';
 define('WSDL', $wsdl);
 
+$error_codes = [
+    '404' => 'Error 404 : Page not found',
+    '2003' => 'Error 2003 : Login server connection down or refused',
+    '1062' => 'Error 1062 : Duplicate entry found',
+    '500' => 'Error 500 : Please report to an admin',
+];
+define('ERROR_CODES', $error_codes);
 //define settings container, based off other setting containers
 //that all appear the same
 
@@ -24,6 +38,12 @@ $settings = [
                 'cache' => false,
                 'auto_reload' => true,
             ],
+        ],
+        "database_settings" => [
+           'HOST' => 'localhost',
+           'USER' => 'apiprojectmanager',
+           'PASS' => '0t4KO0eyKTIijJro',
+           'NAME' => 'apiproject',
         ],
     ],
 ];
