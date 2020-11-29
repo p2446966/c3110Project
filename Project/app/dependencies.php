63dc0ba7517cb565edfb7e474f393bfc82a04f62
p2446966 <58215244+p2446966@users.noreply.github.com>
2020-11-29 13:31:07 +0000

    Added class objects to dependency container

--- Project/app/dependencies.php
+++ Project/app/dependencies.php
@@ -1,5 +1,12 @@
 <?php
 
+require 'vendor\autoload.php';
+
+require 'Project/app/src/Validator.php',
+require 'Project/app/src/SoapWrapper.php',
+require 'Project/app/src/XMLParser.php',
+require 'Project/app/src/SQLQueries.php'
+
 //pre-fab code to register twig templates into slim
 
 $container['view'] = function ($container) {
@@ -19,3 +26,22 @@ $container['view'] = function ($container) {
 };
 
 //register components from src into the container
+$container['validator'] = function ($container) {
+    $validator = new \Telemetry\Validator();
+    return $validator;
+};
+
+$container['soapWrapper'] = function ($container) {
+    $soapWrapper = new \Telemetry\SoapWrapper();
+    return $soapWrapper;
+};
+
+$container['xmlParser'] = function ($container) {
+    $xmlParser = new \Telemetry\XMLParser();
+    return $xmlParser;
+};
+
+$container['SQLQueries'] = function ($container) {
+    $SQLQueries = new \Telemetry\SQLQueries();
+    return $SQLQueries;
+};
