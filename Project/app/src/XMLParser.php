de89ce0775e2f80a14f78d1250ec2e031ab3eed2
p2446966 <58215244+p2446966@users.noreply.github.com>
2020-12-02 16:56:41 +0000

    Created methods for XML Parser

--- Project/app/src/XMLParser.php
+++ Project/app/src/XMLParser.php
@@ -17,7 +17,72 @@ namespace Telemetry;
 
 class XMLParser
 {
-    public function __construct() {}
+    //unparsed array
+    private $XMLData;
+    //processed array
+    private $processed_data;
+    
+    public function __construct()
+    {
+        $this->XMLData = [];
+        $this->processed_data = [];
+    }
+    
     public function __destruct() {}
 
+    //add unparsed data into unparsed array
+    public function setXMLData($recieved_data)
+    {
+        if (!empty($recieved_data) && !is_null($recieved_data))
+        {
+            foreach ($recieved_data as $message)
+            {
+                array_push($this->XMLData, $message);
+            }
+        }
+    }
+    
+    /**
+    * XML format
+    * <sourcemsisdn> number message was sent from
+    * <destinationmsisdn> number message was sent to
+    * <recievedtime> timestamp in format DD/MM/YYYY HH:MM:SS
+    * <message>
+    *       <group> our group, which is TCR
+    *       <switch> 4 digits 1 or 0 indicating on or off for the 4 switches e.g. '0000'
+    *       <fan> direction of fan, either 'forward' or 'reverse'
+    *       <heater> temperature of heater
+    *       <keypad> the last number entered
+    */
+    
+    public function parseXML()
+    {
+        foreach ($this->XMLData as $message)
+        {
+            try
+            {
+                $breakdown = simplexml_load_string($message);
+                // TODO : implement check for user phone number in database
+                // TODO : also add phone number to database
+                //at the moment only messages sent from EE server to EE server can be recieved
+                if ($breakdown->sourcemsisdn == '447817814149')
+                {
+                    if ($breakdown->message->group == 'TCR')
+                    {
+                        array_push($this->processed_data, $breakdown);
+                    }
+                }
+            }
+            catch (Exception $e)
+            {
+                //ignore
+            }
+        }
+    }
+    
+    public function getData()
+    {
+        return $this->processed_data;
+    }   
+        
 }
