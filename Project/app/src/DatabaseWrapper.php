60383c6a205d9a022a06f0f853ac8766269e88a8
p2446966 <58215244+p2446966@users.noreply.github.com>
2020-12-06 13:32:27 +0000

    updated database wrapper

--- Project/app/src/DatabaseWrapper.php
+++ Project/app/src/DatabaseWrapper.php
@@ -17,15 +17,15 @@ class DatabaseWrapper
     public function __construct() {}
     public function __destruct() {}
 
-    public function establishConn()
+    public function establishConn($db_details)
     {
-        $app_settings = $app->getContainer->get('settings');
-        $DB = $app_settings['database_settings'];
+        //$app_settings = $app->getContainer->get('settings');
+        //$DB = $app_settings['database_settings'];
         $this->database = mysqli_connect(
-            $DB['HOST'],
-            $DB['USER'],
-            $DB['PASS'],
-            $DB['NAME']
+            $db_details['HOST'],
+            $db_details['USER'],
+            $db_details['PASS'],
+            $db_details['NAME']
         );
 
         if ($this->database->connect_error)
