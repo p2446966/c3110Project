533dcbb9b5099b8a01b990b8efb30e5f5878d5ce
p2446966 <58215244+p2446966@users.noreply.github.com>
2020-12-06 12:48:15 +0000

    Added refactored login and register routes

---
+++ Project/app/routes/login.php
@@ -0,0 +1,52 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+/// login ///
+$app->get('/Login', function(Request $request, Response $response) use ($app) {
+    session_start();
+    $twigsArray = getPlaceholders($app);
+    return $this->view->render($response, 'login.html.twig', $twigsArray);
+})->setName('Login');
+
+/// auth-login ///
+$app->post('/auth-login', function (Request $request, Response $response) use ($app) {
+    session_start();
+
+    $log = new Logger('logger');
+    $log->pushHandler(new StreamHandler(LOGIN_LOG, Logger::INFO));
+
+    $params = $request->getParsedBody();
+
+    $validator = $app->getContainer()->get('validator');
+    $login_params = $validator->validateInput($params);
+
+    $database = $app->getContainer()->get('SQLQueries');
+    $login_success = $database->loginQuery($login_params['username'], $login_params['password']);
+
+    if ($login_success == true)
+    {
+        $_SESSION['Logged_in'] = true;
+        $log->info('Login Success: ' . $login_params['username']);
+    }
+    else
+    {
+        $_SESSION['Logged_in'] = false;
+        $log->info('Login Attempt: ' . $login_params['username']);
+    }
+
+    $twigsArray = getPlaceholders($app);
+
+    return $this->view->render($response, 'login_results.html.twig', $twigsArray, $login_success);
+
+})->setName('Authorising Login');
+
+public function getPlaceholders($app)
+{
+    $sessions = $app->getContainer()->get('sessionsModel');
+    return $sessions->getStatus();
+}
---
+++ Project/app/routes/register.php
@@ -0,0 +1,52 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+/// Register ///
+$app->get('/register', function (Request $request, Response $response) use ($app) {
+    session_start();
+
+    $twigsArray = getPlaceholders($app);
+    return $this->view->render($response, 'register.html.twig', $twigsArray);
+})->setName('Register');
+
+/// auth-register ///
+$app->post('/auth-register', function (Request $request, Response $response) use ($app) {
+    session_start();
+
+    $log = new Logger('logger');
+    $log->pushHandler(new StreamHandler(REGISTER_LOG, Logger:INFO));
+
+    $params = $request->getParsedBody();
+
+    $validator = $app->getContainer()->get('validator');
+    $cleaned_params = $validator->validateInput($params);
+
+    $database = $app->getContainer()->get('SQLQueries');
+    $register_success = $database->registerQuery($cleaned_params['username'], $cleaned_params['password'], $cleaned_params['email']);
+
+    if ($register_success == true)
+    {
+        $_SESSION['Logged_in'] = true;
+        $log->info('Register success: ' . $cleaned_params['username']);
+    }
+    else
+    {
+        $_SESSION['Logged_in'] = false;
+        $log->info('Register Failure: ' . $cleaned_params['username']);
+    }
+
+    $twigsArray = getPlaceholders($app);
+
+    return $this->view->render($response, 'register_results.html.twig', $twigsArray, $register_success);
+})->setName('Authorising Registration');
+
+public function getPlaceholders($app)
+{
+    $session = $app->getContainer()->get('sessionsModel');
+    return $session->getStatus();
+}
