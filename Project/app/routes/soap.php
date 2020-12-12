89cf56a9bdc9dc8e939168b2feddc2d14433f83e
callum.cunha@gmail.com <callum.cunha@gmail.com>
2020-12-12 16:20:40 +0000

    First progress to displaying soap data.

---
+++ Project/app/routes/soap.php
@@ -0,0 +1,14 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app->get('/soap', function(Request $request, Response $response) use ($app)
+{
+    session_start();
+
+    $twigsArray = $app->getContainer()->get('sessionsModel')->getStatus();
+
+    return $this->view->render($response,
+        'homepage.html.twig', $twigsArray);
+});
