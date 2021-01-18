4eebf8d820cbcdb316e3e8e684edb458aca82358
tejus <p2440241@dmu.ac.uk>
2021-01-18 00:31:13 +0000

    DocBocks done

--- Project/app/routes/login.php
+++ Project/app/routes/login.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * @param Request $request
+ * @param Response $response
+ * @return mixed
+ */
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
@@ -7,7 +11,7 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 /// login ///
-$app->get('/login', function(Request $request, Response $response) use ($app) {
+$app->get( '/login', function(Request $request, Response $response) use ($app) {
     session_start();
     $twigsArray = $app->getContainer()->get('sessionsModel')->getStatus();
     return $this->view->render($response, 'login.html.twig', $twigsArray);
