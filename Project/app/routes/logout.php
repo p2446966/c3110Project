9a302139bed385e4feeae0d2f45f8002835362cc
tejus <p2440241@dmu.ac.uk>
2021-01-18 00:31:49 +0000

    DocBocks done

--- Project/app/routes/logout.php
+++ Project/app/routes/logout.php
@@ -1,6 +1,10 @@
 <?php
 //Doesn't Require SQL.
-
+/**
+ * @param Request $request
+ * @param Response $response
+ * @return mixed
+ */
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
